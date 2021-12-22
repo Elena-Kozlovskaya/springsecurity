e19002a4b823a319a40ac0e8c2b5e04180733c9a
Elena Kozlovskaya <lenadk23@gmail.com>
2021-12-22 16:10:55 +0500

    Added lesson_11 classwork

---
+++ springsecurity/src/main/java/com/kozlovskaya/springsecurity/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kozlovskaya.springsecurity.repositories;
+
+import com.kozlovskaya.springsecurity.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
